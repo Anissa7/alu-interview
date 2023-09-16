450c35f26df49b7c4fa7fd128ea07a57218d92e4
Anissa7 <a.ouedraogo@alustudent.com>
2023-09-16 12:05:25 +0000

    the repos

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-main file for testing
+Main file for testing
 """
 
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
-print("Min # of opereations to reach {} char: {}".format(n,ninOperations(n)))
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
