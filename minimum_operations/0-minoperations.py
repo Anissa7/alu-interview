f8173a0c63e54db3416e9dd1667b22fbc6bd6206
Anissa7 <a.ouedraogo@alustudent.com>
2023-09-16 11:47:05 +0000

    the test hoo

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
+!#/usr/bin/python3
 """
-Main file for testing
+main file for testing
 """
 
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("Min # of opereations to reach {} char: {}".format(n,ninOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
